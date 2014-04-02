bf16ed1c6f2ce70d4370d267b5a118c9a6f490f8
Pierre Massat <pierre@dakis.com>
2014-04-02 17:09:42 -0400

    Add tests

---
+++ lru_test.go
@@ -0,0 +1,180 @@
+package lru
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func TestInitialState(t *testing.T) {
+	cache := New(1, "/tmp")
+	defer cache.Clear()
+
+	if currentSize := cache.CurrentSize(); currentSize != 0 {
+		t.Errorf("size = %v, want 0", currentSize)
+	}
+
+	if maxSize := cache.MaxSize(); maxSize != 1 {
+		t.Errorf("maxSize = %v, want 1", maxSize)
+	}
+}
+
+func TestSetInsertsValue(t *testing.T) {
+	key := "lru.go"
+	data, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	cache := New(1, "/tmp")
+	defer cache.Clear()
+
+	cache.Set(key, data)
+
+	value, ok := cache.Get(key)
+
+	if !ok || !bytes.Equal(value, data) {
+		t.Errorf("Cache has incorrect value")
+	}
+}
+
+func TestSetUpdatesSize(t *testing.T) {
+	cache := New(1, "/tmp")
+	defer cache.Clear()
+
+	emptyValue := []byte{}
+	key := "key1"
+
+	cache.Set(key, emptyValue)
+
+	if currentSize := cache.CurrentSize(); currentSize != 0 {
+		t.Errorf("cache.currentSize() = %v, expected 0", currentSize)
+	}
+
+	key = "lru.go"
+	someValue, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	fileSize := uint64(len(someValue))
+
+	cache.Set(key, someValue)
+
+	if currentSize := cache.CurrentSize(); currentSize != fileSize {
+		t.Errorf("cache.currentSize() = %v, expected %v", currentSize, fileSize)
+	}
+}
+
+func TestGetNonExistent(t *testing.T) {
+	cache := New(1, "/tmp")
+
+	if _, ok := cache.Get("i don't exist"); ok {
+		t.Error("Cache returned a crap value after no inserts.")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	key := "lru.go"
+	value, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	cache := New(1, "/tmp")
+	defer cache.Clear()
+
+	if cache.Delete(key) {
+		t.Error("Item unexpectedly already in cache.")
+	}
+
+	cache.Set(key, value)
+
+	if !cache.Delete(key) {
+		t.Error("Expected item to be in cache.")
+	}
+
+	if currentSize := cache.CurrentSize(); currentSize != 0 {
+		t.Errorf("cache.CurrentSize() = %v, expected 0", currentSize)
+	}
+
+	if _, ok := cache.Get(key); ok {
+		t.Error("Cache returned a value after deletion.")
+	}
+}
+
+func TestClear(t *testing.T) {
+	key := "lru.go"
+	value, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	cache := New(1, "/tmp")
+	defer cache.Clear()
+
+	cache.Set(key, value)
+	cache.Clear()
+
+	if currentSize := cache.CurrentSize(); currentSize != 0 {
+		t.Errorf("cache.CurrentSize() = %v, expected 0 after Clear()", currentSize)
+	}
+}
+
+func TestCapacityIsObeyed(t *testing.T) {
+	key := "lru.go"
+	value, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	size := uint64(len(value)) * 3
+	cache := New(size, "/tmp")
+	defer cache.Clear()
+
+	cache.Set("key1", value)
+	cache.Set("key2", value)
+	cache.Set("key3", value)
+
+	if currentSize := cache.CurrentSize(); currentSize != size {
+		t.Errorf("cache.CurrentSize() = %v, expected %v", currentSize, size)
+	}
+
+	cache.Set("key4", value)
+
+	if currentSize := cache.CurrentSize(); currentSize != size {
+		t.Errorf("post-evict cache.CurrentSize() = %v, expected %v", currentSize, size)
+	}
+}
+
+func TestLRUIsEvicted(t *testing.T) {
+	key := "lru.go"
+	value, err := ioutil.ReadFile(key)
+
+	if err != nil {
+		t.Errorf("Couldn't read %v", key)
+	}
+
+	size := uint64(len(value)) * 3
+	cache := New(size, "/tmp")
+	defer cache.Clear()
+
+	cache.Set("key1", value)
+	cache.Set("key2", value)
+	cache.Set("key3", value)
+
+	cache.Get("key3")
+	cache.Get("key2")
+	cache.Get("key1")
+
+	cache.Set("key0", value)
+
+	if _, ok := cache.Get("key3"); ok {
+		t.Error("Least recently used element was not evicted.")
+	}
+}
